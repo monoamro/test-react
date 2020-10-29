35fe9a06b15ebac6c267459a7b1d0011d57649b0
Montaser Amro <mono.amro@gmail.com>
2020-10-29 17:40:02 +0100

    calls API and gets user data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import React, { useEffect, useState } from 'react';
+
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/users/1")
+         .then((res) => {
+           const { data } = res;
+           console.log(data);
+          })
+         .catch((e) => {console.log(e)});
+  }, []);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
